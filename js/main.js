9b0e94dbfbefc40a6cd70bda9b4d009b8fd49f7b
N4LT4M <m4tt3o.casale@gmail.com>
2023-07-06 15:27:30 +0200

    inizio esercizio

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/*Utilizzare la funzione sommaNumeri per stampare il risultato.
+L'array di N numeri da passare alla funzione lo create chiedendo all'utente 
+quanti numeri vuole inserire (cioè N),
+e poi facendogli inserire questi N numeri all'utente stesso
+tramite apposito prompt().
+*/
+
+//chiedere tramite prompt all'utente quanti numeri vuole che vengano generati (n)
+
+// fare un array lungo quanto (n) digitato dall'utente composto da numeri scelti dall'utente
